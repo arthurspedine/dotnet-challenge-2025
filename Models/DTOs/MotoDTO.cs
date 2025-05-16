7477678ba859d5d1c9eef101f8a400e7f00cd17b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-16 19:51:16 -0300

    adicionando service e dto da moto

---
+++ Models/DTOs/MotoDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Motoflow.Models.DTOs
+{
+    public class MotoDTO
+    {
+        public long Id { get; set; }
+        public MotoType Type { get; set; }
+
+        public string? Placa { get; set; }
+        public string? Chassi { get; set; }
+        public string? QRCode { get; set; }
+
+        public long? HistoricoAtivoId { get; set; }
+    }
+}
--- Repositories/MotoRepository.cs
+++ Repositories/MotoRepository.cs
@@ -17,5 +17,34 @@ namespace Motoflow.Repositories
         {
             return await _context.Motos.ToListAsync();
         }
+
+        public async Task<Moto?> GetByIdAsync(long id)
+        {
+            return await _context.Motos
+                .Include(m => m.Historicos) // Inclui relacionamentos, se necessário
+                .FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task AddAsync(Moto moto)
+        {
+            _context.Motos.Add(moto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Moto moto)
+        {
+            _context.Motos.Update(moto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(long id)
+        {
+            var moto = await _context.Motos.FindAsync(id);
+            if (moto != null)
+            {
+                _context.Motos.Remove(moto);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
